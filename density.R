68ce0b4823a99bbcb10059f1d1a5eba07da7bc11
Gene Leynes <gleynes@gmail.com>
2017-03-17 16:50:06 -0500

    working

--- density.R
+++ density.R
@@ -1,6 +1,9 @@
 
 rm(list = ls())
 source("density_example.R")
+source("R/plenario_datadump.R")
+source("R/kde.R")
+source("R/interpolate_xy.R")
 
 
 library(geneorama)
@@ -32,32 +35,6 @@ event_window <- records[ , c(min(date) - max(days_back),
                              max(date))]
 
 
-plenario_datadump <- function(dataset = "crimes_2001_to_present", 
-                              filter = "", 
-                              event_window){
-    
-    if(filter != ""){
-        filter <- paste0(filter, "&", collapse = "&")
-    }
-    
-    url <- paste0("http://plenar.io/v1/api/datadump/?",
-                  sprintf("dataset_name=%s&", dataset),
-                  filter,
-                  sprintf("obs_date__ge=%s&", event_window[1]),
-                  sprintf("obs_date__le=%s", event_window[2]))
-    cat("downloading: ", url, "\n")
-    data <- httr::GET(url)
-    if(httr::http_status(data)$category != "Success"){
-        stop("http error")
-    } else {
-        contentdata <- httr::content(data, as = "text")
-        contentdata <- jsonlite::fromJSON(contentdata)
-        feats <- data.table(contentdata$features$properties)
-        feats <- feats[ , point_date := as.IDate(point_date)][]
-    }
-        
-    return(feats)
-}
 
 event_data <- plenario_datadump(filter = "primary_type=BATTERY", event_window=event_window)
 event_data
